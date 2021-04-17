d89b9752ee6a2b4ac7ec9e8aaa9498d03834eba2
Luis Falcon <falcon@gnuhealth.org>
2021-04-17 18:42:56 +0100

    Add Marco Gebhardt in credits

--- mygnuhealth/about.py
+++ mygnuhealth/about.py
@@ -27,6 +27,7 @@ __thanks__ = "Axel Braun\n" \
              "Paul Brown\n" \
              "Yuri Chornoivan\n" \
              "Leonardo D'Acchille\n" \
+             "Marco Gebhardt" \
              "Riccardo Iaconelli\n" \
              "Cristian Maureira-Fredes\n" \
              "Karl Ove Hufthammer\n" \
